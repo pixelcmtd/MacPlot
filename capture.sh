80c6f37b80d0312914abb2e21ca06b5f7a166890
chrissx <chrissx@chrissx.de>
2021-02-17 09:20:18 +0100

    wrote a script for capturing that actually works

--- capture.sh
+++ capture.sh
@@ -1,12 +1,3 @@
-sample() (powermetrics -i 1000 -n 1 --show-all -f plist)
-plist2json() (python3 plist2json.py)
-
-file="$(date).plist"
-{
-        echo -n '['
-        while true ; do
-                echo -n "$(sample | plist2json),"
-        done
-        echo -n ']'
-} > "$file"
+file="$(date).json"
+python3 capturescript.py "$file"
 chown "$(logname)" "$file"
