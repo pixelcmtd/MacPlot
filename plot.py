36abca4d4b5c4e19d963297ed230e89e412af8c9
chrissx <chrissx@chrissx.de>
2021-02-21 15:21:08 +0100

    now we also need new plotting code

--- plot.py
+++ plot.py
@@ -3,17 +3,22 @@ import json
 from sys import argv
 import csv
 import subprocess
+from functools import reduce
 
 samples = json.load(open(argv[1] + '.json', 'r'))
 w = csv.writer(open(argv[1] + '.csv', 'w'))
 
-w.writerow(['time (s)', 'power (mW)', 'fan (rpm)'])
+w.writerow(['time (s)', 'cpu (‰)', 'fan (rpm)', 'P-cluster temp (*10C)'])
 
 for sample in samples:
-    row = [float(sample['timestamp']) / 1000]
-    row.append(int([i for i in sample['powermetrics'] if i[0] == 'Package' and i[1] == 'Power'][0][2]))
-    row.append(int([i for i in sample['spindump'] if i[0] == 'Fan' and i[1] == 'speed'][0][2]))
+    istat = sample['istatistica']
+    temps = istat['sensors']
+    temps = [temps[k] for k in temps.keys() if k.split(' ')[0] == 'pACC']
+    temps = [int(i) for i in temps if i != '' and int(i) != 0]
+    row = ['%.1f' % sample['timestamp']]
+    row.append(int(istat['summary_cpuLoad'] * 1000))
+    row.append(istat['sensors_fansData'].rstrip())
+    row.append(int(reduce(lambda x, y: x + y, temps) / len(temps) * 10))
     w.writerow(row)
-
 
 subprocess.run(['/bin/sh', '-c', 'open \'' + argv[1] + '.csv\''])
